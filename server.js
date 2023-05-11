65e47e038e7503d3e627cacc09f928a274e15a0f
kalen510 <kalen.wiley@gmail.com>
2023-05-10 20:05:26 -0400

    set up express server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// instantiate server
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'));
+
+// connect to mongoose on start
+mongoose.connect('mongodb://localhost:27017/sonet-API', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// log mongo queries being executed
+mongoose.set('debug', true);
+
+// listen for request on server
+app.listen(PORT, () => console.log(`Connected to localhost:${PORT}`));
