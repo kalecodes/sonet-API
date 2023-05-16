1095da06b720f07d4ab82192a76aee49dab4c1df
kalen510 <kalen.wiley@gmail.com>
2023-05-16 15:12:57 -0400

    thought routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
 // add prefix of /users to routes
 router.use('/users', userRoutes);
+// add prefix of /thoughts to routes
+router.use('/thoughts', thoughtRoutes);
 
 
 module.exports = router;
