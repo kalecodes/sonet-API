fafbf11711997925173757d444f2eca4f9188429
kalen510 <kalen.wiley@gmail.com>
2023-05-16 13:52:37 -0400

    friend controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -65,7 +65,7 @@ const userController = {
         User.findOneAndDelete({ _id: params.id })
             .then(dbUserData => {
                 if (!dbUserData) {
-                    res.status(404).json({ message: 'No user found with this id!'})
+                    res.status(404).json({ message: 'No user found with this id!'});
                     return;
                 }
                 res.json(dbUserData);
@@ -74,7 +74,40 @@ const userController = {
                 console.log(err);
                 res.status(400).json(err);
             });
+    },
+
+    // add friend (post)
+    addFriend({ params }, res) {
+        User.findOneAndUpdate({ _id: params.userId }, { $push: { friends: { friendId: params.friendId } } }, { new: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // delete friend 
+    removeFriend({ params }, res) {
+        User.findOneAndUpdate({ _id: params.userId }, { $pull: { friends: { friendId: params.friendId } } }, { new: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
     }
+
 };
 
 module.exports = userController;
